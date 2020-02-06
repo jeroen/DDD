77132771173db21090503fbd8fb6ec6668517d92
Jeroen Ooms <jeroenooms@gmail.com>
2020-02-06 22:51:29 +0100

    Try to fix 32-bit on Windows

--- tests/testthat/test_DDD.R
+++ tests/testthat/test_DDD.R
@@ -40,23 +40,23 @@ test_that("DDD works", {
   
   testthat::expect_equal(r5,r6,tolerance = .00001)
   testthat::expect_equal(r5,r7,tolerance = .01)
-  expect_equal_x64(-8.582413,r7,tolerance = .00001)
+  expect_equal(-8.582413,r7,tolerance = .00001)
 
   pars1 = c(0.2,0.05,1000000)
   pars2 = c(1000,1,1,0,0,2)
   brts = 1:10
   r8 <- DDD:::dd_loglik_test(pars1 = pars1,pars2 = pars2,brts = brts,missnumspec = missnumspec,rhs_func_name = 'dd_loglik_rhs_FORTRAN',methode = methode)
   r9 <- DDD:::dd_loglik_test(pars1 = c(pars1[1:2],Inf),pars2 = pars2,brts = brts,missnumspec = missnumspec,rhs_func_name = 'dd_loglik_rhs_FORTRAN',methode = methode)
-  expect_equal_x64(r8,r9,tolerance = .00001)
+  expect_equal(r8,r9,tolerance = .00001)
   
   pars1 <- c(0.2,0.05,15)
   pars2 <- c(4,1,0,0,2)
   brts <- 1:10
   r10 <- DDD::bd_loglik(pars1 = pars1,pars2 = pars2,brts = brts,missnumspec = 0,methode = 'lsoda')
-  expect_equal_x64(r10,-11.6076802136507471,tolerance = 1E-16)
+  expect_equal(r10,-11.6076802136507471,tolerance = 1E-10)
 
   r11 <- DDD::bd_loglik(pars1 = c(0.4,0.1,20),pars2 = c(4,0,1,0,2), brts = 1:10, missnumspec = 0)
-  expect_equal_x64(r11,-27.7337684064852610,tolerance = 1E-16)
+  expect_equal(r11,-27.7337684064852610,tolerance = 1E-10)
   
   pars <- c(0.4, 0.1, 0.6, 0.2)
   brts <- list(10:6, 3:2)
